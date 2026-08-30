fc704573e4f9c048b2305cd0533b2f4ce75bf109
beco1409-spec <beco1409@gmail.com>
2026-08-30 09:21:17 -0300

    Corrige build do vite e importacao do cifra club

--- src/lib/cifraclub.functions.ts
+++ src/lib/cifraclub.functions.ts
@@ -1,5 +1,6 @@
-import { createServerFn } from '@tanstack/start'
+import { createServerFn } from '@tanstack/react-start'
 import { z } from 'zod'
+import { parseCifraClubHtml } from './cifraclub-parser'
 
 export const getCifraClubSong = createServerFn({ method: 'GET' })
   .validator((data: unknown) => {
@@ -16,4 +17,22 @@ export const getCifraClubSong = createServerFn({ method: 'GET' })
     }
     const html = await response.text()
     return { html }
+  })
+
+export const importCifraClub = createServerFn({ method: 'GET' })
+  .validator((data: unknown) => {
+    return z
+      .object({
+        url: z.string().url(),
+      })
+      .parse(data)
+  })
+  .handler(async ({ data }) => {
+    const response = await fetch(data.url)
+    if (!response.ok) {
+      throw new Error('Falha ao buscar cifra no Cifra Club')
+    }
+    const html = await response.text()
+    const { data: importado } = parseCifraClubHtml(html)
+    return importado
   })
